741877e0feb565e6d4ce5e843ddda91207577e50
BaoPT <phutuongbao1999@gmail.com>
2021-07-15 17:38:16 +0700

    Update Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,12 +7,21 @@ class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark">
-                <p className="navbar-brand">
-                    Navbar 
-                    {/* <span className="badge badge-pill badge-primary">{this.props.totalCounters}</span> */}
-                </p>
-            </nav>
+            // <nav className="navbar navbar-dark bg-dark">
+            //     <p className="navbar-brand">
+            //         Navbar 
+            //         {/* <span className="badge badge-pill badge-primary">{this.props.totalCounters}</span> */}
+            //     </p>
+            //     <p className="navbar-brand">
+            //         Navbar 
+            //         {/* <span className="badge badge-pill badge-primary">{this.props.totalCounters}</span> */}
+            //     </p>
+            // </nav>
+            <ul className="navbar navbar-dark bg-dark">
+                <p className="nav-link active navbar-brand">Counter</p>
+                <p className="nav-link navbar-brand">TODO</p>
+                <p className="nav-link navbar-brand">TODAY</p>
+            </ul>
         )
     }
 }
