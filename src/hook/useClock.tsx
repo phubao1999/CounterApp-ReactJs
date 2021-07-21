1e1034ed831319a5e11430dc8d3c6f469349a874
BaoPT <phutuongbao1999@gmail.com>
2021-07-21 15:40:53 +0700

    Update testing hook

--- src/hook/useClock.tsx
+++ src/hook/useClock.tsx
@@ -1,46 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const useClock = () => {
+
+export default function useClock() {
     const [time, setTime] = useState("");
-    const [ampm, setampm] = useState("");
 
-    // Function cập nhật thời gian.
-    const updateTime = function () {
-        let dateInfo = new Date();
-        let hour = 0;
-        /* Lấy giá trị của giờ */
-        if (dateInfo.getHours() === 0) {
-            hour = 12;
-        } else if (dateInfo.getHours() > 12) {
-            hour = dateInfo.getHours() - 12;
-        } else {
-            hour = dateInfo.getHours();
-        }
-        /* Lấy giá trị của phút */
-        let minutes =
-            dateInfo.getMinutes() < 10
-                ? parseInt("0" + dateInfo.getMinutes())
-                : dateInfo.getMinutes();
-
-        /* Lấy gía trị của giây */
-        let seconds =
-            dateInfo.getSeconds() < 10
-                ? "0" + dateInfo.getSeconds()
-                : dateInfo.getSeconds();
-
-        /* Định dạng ngày */
-        let ampm = dateInfo.getHours() >= 12 ? "PM" : "AM";
-
-        /* Cập nhật state */
-        setampm(ampm)
-        setTime(`${hour}:${minutes}:${seconds}`);
+    const updateTimeNow = () => {
+        const d = new Date();
+        const hour = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+        const minutes = d.getMinutes();
+        const timeMockUp = d.getHours() > 12 ? "PM" : "AM";
+        return `${hour}:${minutes} ${timeMockUp}`
     };
 
-    setInterval(function () {
-        updateTime();
-    }, 1000);
+    useEffect(() => {
+        setTime(updateTimeNow());
+    }, []);
 
-    return [time, ampm];
+    return time;
 }
-
-export default useClock;
