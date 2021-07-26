d50cad6888c60f762ac33da25835ae03c72d680e
BaoPT <phutuongbao1999@gmail.com>
2021-07-26 11:35:16 +0700

    Send Msg Components

--- src/components/pages/pageSocket/pageSocket.tsx
+++ src/components/pages/pageSocket/pageSocket.tsx
@@ -1,5 +1,10 @@
+import SendMSG from "./SendMSG/sendMSG";
+
 export default function PageSocket() {
     return (
-        <h1>Socket Chat Demo</h1>
+        <div>
+            <h1 className="ml-4">Socket Chat Demo</h1>
+            <SendMSG />
+        </div>
     )
 }
